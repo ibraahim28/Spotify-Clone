e715b00a3bbe6dd813c48653e71a90e2c0eb1d54
ibu-ibra <ibraahimimran23@gmail.com>
2024-05-14 11:09:13 +0500

    added song list and some small changes

--- spotify.js
+++ spotify.js
@@ -1,11 +1,11 @@
 
-async function main () {
+async function getSongs () {
     let a = await fetch("http://127.0.0.1:5500/songs/");
     let response = await a.text();
-    console.log(response);
     let div = document.createElement("div")
     div.innerHTML = response;
     let as = div.getElementsByTagName("a")
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul");
     let songs = [];
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
@@ -13,8 +13,18 @@ async function main () {
         if (element.href.endsWith(".mp3")) {
             songs.push(element.href)
         }
+
     }
-    console.log(songs)
+    return songs;
+}
+
+async function main () {
+    let songs = await getSongs();
+    console.log(songs);
+
+    var audio = new Audio(songs[0]);
+    
+    audio.play;
 }
 
 main();
