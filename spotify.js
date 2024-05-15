a3168d888e6a97d15502f94988e2ef317e3107ab
ibu-ibra <ibraahimimran23@gmail.com>
2024-05-15 23:00:10 +0500

    added song names into library

--- spotify.js
+++ spotify.js
@@ -1,30 +1,35 @@
-
 async function getSongs () {
     let a = await fetch("http://127.0.0.1:5500/songs/");
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
-    let as = div.getElementsByTagName("a")
-    let songUL = document.querySelector(".songList").getElementsByTagName("ul");
+    let as = div.getElementsByTagName("a");
+
     let songs = [];
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
 
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href)
+            songs.push(element.href.split(`/songs/`)[1]);
         }
 
     }
     return songs;
+    
 }
 
 async function main () {
     let songs = await getSongs();
     console.log(songs);
 
-    var audio = new Audio(songs[0]);
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+    songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll('%20', " ")} </li>`;
+}
+
+    var audio = new Audio (songs[0]);
     
-    audio.play;
+    audio.play();
 }
 
 main();
