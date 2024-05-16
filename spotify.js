ce743ae480367e893882dc0a612574f56b865007
ibu-ibra <ibraahimimran23@gmail.com>
2024-05-16 10:54:25 +0500

    Made proper song cards in Library and added a playbar

--- spotify.js
+++ spotify.js
@@ -1,35 +1,41 @@
-async function getSongs () {
-    let a = await fetch("http://127.0.0.1:5500/songs/");
-    let response = await a.text();
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a");
+async function getSongs() {
+  let a = await fetch("http://127.0.0.1:5500/songs/");
+  let response = await a.text();
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
 
-    let songs = [];
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
+  let songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
 
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split(`/songs/`)[1]);
-        }
-
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split(`/songs/`)[1]);
     }
-    return songs;
-    
+  }
+  return songs;
 }
 
-async function main () {
-    let songs = await getSongs();
-    console.log(songs);
+async function main() {
+  let songs = await getSongs();
+  console.log(songs);
 
-    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
-    for (const song of songs) {
-    songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll('%20', " ")} </li>`;
-}
+  let songUL = document
+    .querySelector(".songList")
+    .getElementsByTagName("ul")[0];
+  for (const song of songs) {
+    songUL.innerHTML =
+      songUL.innerHTML + `<li> <img class="invert" src="/SVG/Music.svg" alt="Music">
+                <div class="info">
+                  <div>${song.replaceAll("%20", " ")}</div>
+                  <div>Ibra</div>
+                </div>
+                <img class="invert" src="/SVG/play.svg" alt="Play"> </li>`;
+  }
 
-    var audio = new Audio (songs[0]);
-    
-    audio.play();
+  var audio = new Audio(songs[0]);
+
+  audio.play();
 }
 
 main();
