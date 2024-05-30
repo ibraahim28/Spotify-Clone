96059e7e5ba265b0501da326711b90fe83073582
ibu-ibra <ibraahimimran23@gmail.com>
2024-05-30 11:13:54 +0500

    added funtionality to seek bar and some other small things

--- spotify.js
+++ spotify.js
@@ -3,6 +3,21 @@ let prevbtn = document.getElementById("prev");
 let playbtn = document.getElementById("play");
 let nextbtn = document.getElementById("next");
 let mutebtn = document.getElementById("mute");
+
+function secondsToMinutesSeconds(seconds) {
+  if (isNaN(seconds) || seconds < 0) {
+      return "00:00";
+  }
+
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:5500/songs/");
   let response = await a.text();
@@ -77,6 +92,22 @@ const playMusic = (track, pause = false) => {
     currentSong.play();
     playbtn.src = "/SVG/pause.svg"
   }
+
+  document.querySelector(".song-info").innerHTML = decodeURI(track);
+  document.querySelector(".song-time").innerHTML = "00:00 : 00:00"
 }
 
+//add functionality to the circle
+currentSong.addEventListener("timeupdate", ()=> {
+  document.querySelector(".song-time").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} : ${secondsToMinutesSeconds(currentSong.duration)}`;
+
+  document.querySelector(".circle").style.left = currentSong.currentTime / currentSong.duration * (100) + "%";
+})
+
+//add an event listener to seekbar
+document.querySelector(".seek-bar").addEventListener("click", e=>{
+  let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+  document.querySelector(".circle").style.left = percent + "%";
+  currentSong.currentTime = ((currentSong.duration)*percent) / 100;
+})
 main();
